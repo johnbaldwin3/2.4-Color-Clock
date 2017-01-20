bcf18ba7910157cb1ff5796cf63645cb21671c8f
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-01-20 17:57:16 -0500

    colors working

--- main.js
+++ main.js
@@ -13,6 +13,12 @@ console.log("say stuff");
   var minutes= document.querySelector('.the-time .minutes');
   var seconds = document.querySelector('.the-time .seconds');
   var timeBar = document.getElementById('timer-bar');
+  var docBody = document.querySelector('body');
+
+  var hexSeconds;
+  var hexMinutes;
+  var hexHours;
+  var hexColor;
 
 window.setInterval(currentTime, 1000);
 
@@ -28,7 +34,7 @@ function currentTime(){
   minutesVar = getTime.getMinutes();
   secondsVar = getTime.getSeconds();
 
-  console.log('mady', ("0" + secondsVar.toString("16")).slice(-2));
+  // console.log('mady', ("0" + secondsVar.toString("16")).slice(-2));
 
   hours.textContent = ("0" + hoursVar).slice(-2);
   minutes.textContent = ("0" + minutesVar).slice(-2);
@@ -39,7 +45,18 @@ function currentTime(){
   timeBar.style.width = secondsVar.toString() + "%";
   //
   // console.log(secondsVarDec);
+  hexSeconds = ("0" + secondsVar.toString("16")).slice(-2);
+  hexMinutes = ("0" + minutesVar.toString("16")).slice(-2);
+  hexHours = ("0" + hoursVar.toString("16")).slice(-2);
 
+  hexColor = ('#' + hexHours + hexMinutes + hexSeconds);
+
+//console was able to log hex value for seconds
+  // console.log(hexSeconds, "john");
+  // able to log hexColor
+  console.log(hexColor, "john");
+
+  docBody.style.backgroundColor = hexColor;
 
 }
 
